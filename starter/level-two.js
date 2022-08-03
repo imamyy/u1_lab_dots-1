9451bc048d9714b547307e8667c706c6cc39db4e
imamyy <amytran2608@gmail.com>
2022-08-03 17:47:35 -0400

    Added level two for the game

--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,19 @@
  * DOTS: Level Two
  *
  */
+let score = 0;
+const ball = document.querySelectorAll(".js-ball");
+console.log(ball);
+const scoreDisplay = document.querySelector(".js-score");
+console.log(scoreDisplay);
+const levelWinner = document.querySelector(".level-winner");
+console.log(levelWinner);
+for (let i = 0; i < ball.length; i++) {
+  ball[i].addEventListener("click", () => {
+    score += 10;
+    scoreDisplay.innerText = score;
+    if (score >= 100) {
+      levelWinner.style.opacity = 1;
+    }
+  });
+}
