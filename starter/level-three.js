d9d1a96a01457b38503eae6ac2944af3087447b8
imamyy <amytran2608@gmail.com>
2022-08-03 19:54:21 -0400

    Added level three of the game

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,35 @@
  * DOTS: Level Three
  *
  */
+let score = 0;
+const ball = document.querySelectorAll(".js-ball");
+console.log(ball);
+const scoreDisplay = document.querySelector(".js-score");
+console.log(scoreDisplay);
+const levelWinner = document.querySelector(".level-winner");
+console.log(levelWinner);
+
+ball[0].addEventListener("click", () => {
+  let scoreIncrement = parseInt(ball[0].getAttribute("data-increment"), 10);
+  score += scoreIncrement;
+  scoreDisplay.innerText = score;
+  if (score >= 100) {
+    levelWinner.style.opacity = 1;
+  }
+});
+ball[1].addEventListener("click", () => {
+  let scoreIncrement = parseInt(ball[1].getAttribute("data-increment"), 10);
+  score += scoreIncrement;
+  scoreDisplay.innerText = score;
+  if (score >= 100) {
+    levelWinner.style.opacity = 1;
+  }
+});
+ball[2].addEventListener("click", () => {
+  let scoreIncrement = parseInt(ball[2].getAttribute("data-increment"), 10);
+  score += scoreIncrement;
+  scoreDisplay.innerText = score;
+  if (score >= 100) {
+    levelWinner.style.opacity = 1;
+  }
+});
