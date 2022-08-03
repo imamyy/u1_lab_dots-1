974b29f0a903bfec780056863ed4d4b6a135b230
imamyy <amytran2608@gmail.com>
2022-08-03 17:21:28 -0400

    Added level-one.js for Dots

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,18 @@
  * DOTS: Level One
  *
  */
+let score = 0;
+const ball = document.querySelector(".js-ball");
+console.log(ball);
+const scoreDisplay = document.querySelector(".js-score");
+console.log(scoreDisplay);
+const levelWinner = document.querySelector(".level-winner");
+console.log(levelWinner);
+ball.addEventListener("click", () => {
+  score += 10;
+  scoreDisplay.innerText = score;
+  if (score >= 100) {
+    // console.log("Congrats!You have beaten level 1.");
+    levelWinner.style.opacity = 1;
+  }
+});
